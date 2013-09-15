64841eee13d17c93ae9ae37d646572a06a5f3083
Alex Mingoia <talk@alexmingoia.com>
2013-09-14 21:15:51 -0700

    Add component.js

---
+++ component.js
@@ -0,0 +1,11 @@
+{
+  "name": "context-access",
+  "description": "Powerful access control with a dead simple API.",
+  "version": "0.0.1",
+  "keywords": ["acl", "permissions", "access control", "permission", "access"],
+  "repo": "bloodhound/context-access",
+  "scripts": [
+    "index.js"
+  ],
+  "license": "MIT"
+}
